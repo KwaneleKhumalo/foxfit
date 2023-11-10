17bf042d8fa75f1edfb3e921ff9f9a70c986f07b
Kwanele <kwanele@Kwaneles-MacBook-Pro.local>
2023-11-10 15:59:20 -0700

    touch ups

--- server/controllers/WorkoutPlansController.js
+++ server/controllers/WorkoutPlansController.js
@@ -1,38 +1,15 @@
 import Workout from "../models/workoutsModel.js"
 import { populateField } from "../utils/populateFields.js"
 
-// export const createWorkout = async (req, res) => {
-//   try {
-//     const user = req.user._id.toString()
-//     const { name, description, sets, repetitions, weightPerSet } = req.body
 
-//     const workoutPlan = await Workout.create({
-//       user,
-//       name,
-//       description,
-//       sets,
-//       repetitions,
-//       weightPerSet
-//     })
-
-//     if (!workoutPlan) {
-//       return res.status(400).json({ message: "Failed to create workout plan." })
-//     }
-
-//     return res.status(201).json({ message: "Workout plan created successfully.", workoutPlan })
-//   } catch (error) {
-//     console.error("Error creating workout plan:", error)
-//     return res.status(500).json({ message: "Internal Server Error. Please try again!" })
-//   }
-// }
 
 export const createWorkout = async (req, res) => {
   try {
-    const user = req.user._id.toString() // Use subscriber instead of user
+    const user = req.user._id.toString() 
     const { name, description, sets, repetitions, weightPerSet } = req.body
 
     const workoutPlan = await Workout.create({
-      user, // Use the correct field name
+      user,
       name,
       description,
       sets,
@@ -50,8 +27,6 @@ export const createWorkout = async (req, res) => {
     return res.status(500).json({ message: "Internal Server Error. Please try again!" })
   }
 }
-
-
 
 export const getUserWorkoutPlan = async (req, res) => {
   try {
